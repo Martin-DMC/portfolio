2438b83b065296164b545c3ee93894185071e52e
Martin-DMC <the.chiperfil@gmail.com>
2026-01-09 22:03:02 -0300

    feat: add lighthouse to know the performance

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import './layout.css';
 	
 	let { children } = $props();
+	injectSpeedInsights();
 </script>
 
 {@render children()}
+
